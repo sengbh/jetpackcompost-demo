f9d87ff3bb450a3c97591bd7b813a8c8f86c060e
sengbh <seng4510@gmail.com>
2024-04-18 16:38:13 -0500

    -Implement remember and mutableStateOf

--- app/src/main/java/com/example/jetpackcompost_demo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompost_demo/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.jetpackcompost_demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompost_demo.ui.theme.JetpackcompostdemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +22,13 @@ class MainActivity : ComponentActivity() {
             JetpackcompostdemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.padding()
+                    ){
+                        CounterApp()
+                    }
                 }
             }
         }
@@ -27,17 +36,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackcompostdemoTheme {
-        Greeting("Android")
+fun CounterApp(){
+    var count by remember { mutableIntStateOf(0) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text("The current count is $count")
+        Row(modifier = Modifier.padding(8.dp)){
+            Button(onClick = { count++ }) {
+                Text("Add")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = { count-- }) {
+                Text("Subtract")
+            }
+        }
     }
 }
