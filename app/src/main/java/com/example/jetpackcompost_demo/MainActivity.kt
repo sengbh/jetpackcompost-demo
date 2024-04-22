1d8f372c27c9ee41b104b28089fb2525e5b92d50
sengbh <seng4510@gmail.com>
2024-04-22 13:22:46 -0500

    -Add basic textfield etc

--- app/src/main/java/com/example/jetpackcompost_demo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompost_demo/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.jetpackcompost_demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcompost_demo.ui.theme.JetpackcompostdemoTheme
 
@@ -24,10 +27,10 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier.padding()
+                        verticalArrangement = Arrangement.Center
                     ){
-                        CounterApp()
+                        Column(modifier = Modifier.padding(bottom = 8.dp)){ CounterApp() }
+                        Column(){ UILayout() }
                     }
                 }
             }
@@ -40,7 +43,7 @@ fun CounterApp(){
     var count by remember { mutableIntStateOf(0) }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth().background(Color.LightGray)
     ) {
         Text("The current count is $count")
         Row(modifier = Modifier.padding(8.dp)){
@@ -52,5 +55,35 @@ fun CounterApp(){
                 Text("Subtract")
             }
         }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun UILayout() {
+    var textField by remember { mutableStateOf("") }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth().background(Color.Gray)) {
+        TextField(
+            value = textField,
+            onValueChange = {textField = it},
+            label = { Text("Enter something here") },
+            placeholder = { Text("Name") },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = {
+//                    LocalFocusManager.current.moveFocus(FocusDirection.Down)
+//                    LocalSoftwareKeyboardController.current?.hide()
+                }
+            )
+        )
+
+        Button(onClick = {}){
+            Text("Click Me")
+        }
+        Text(
+            "Welcome To Jetpack Compose!",
+            style = MaterialTheme.typography.headlineSmall
+        )
     }
 }
