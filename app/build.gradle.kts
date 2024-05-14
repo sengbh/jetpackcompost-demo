747ae12addfbbf44df4c15405dc68d0a9437489b
sengbh <seng4510@gmail.com>
2024-05-14 01:36:47 -0500

    -Add launcher logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.compose.material:material-icons-extended:1.6.6")
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.compose.material:material-icons-extended:1.6.7")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.google.firebase:protolite-well-known-types:18.0.0")
     implementation("androidx.leanback:leanback:1.0.0")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
